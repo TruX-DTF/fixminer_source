9f11458a5a40635222108d31ca09e94f94d5e271
fixminer <fixminer@fixminer.com>
2020-04-09 14:35:58 +0200

    travis fix try6

--- .travis_native_dependencies.sh
+++ .travis_native_dependencies.sh
@@ -1,6 +1,6 @@
 # 0) Installing dependencies
 sudo apt-get update
-sudo apt-get install gdebi-core libarchive13
+sudo apt-get install gdebi-core libarchive-tools
 
 wget http://131.123.42.38/lmcrs/v1.0.0/srcml_1.0.0-1_ubuntu18.04.deb
 sudo gdebi srcml_1.0.0-1_ubuntu18.04.deb -n
